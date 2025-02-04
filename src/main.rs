6b02d885fd8a2d2de7dd41143316eca3e1168794
Olaf Flebbe <of@oflebbe.de>
2025-02-04 22:22:35 +0100

    feat: first ui

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod pixmap;
 const TEXTURE_MAX_SIZE: usize = 16384;
 
 fn load_images(
-    input: &input::Input
+    input: &input::Input, fft_size:usize, overlap : f32
 ) -> (
     epaint::image::ColorImage,
     epaint::image::ColorImage,
@@ -19,8 +19,8 @@ fn load_images(
 ) {
     let data16 = input.get();
     let channels = &input.channels;
-    let fft_size = 512;
-    let step_size = (fft_size as f32 / 0.95f32) as usize;
+ 
+    let step_size = (fft_size as f32 * (1.0 - overlap)) as usize;
 
     let width = ((data16.len() - channels[0].offset) / channels[0].scale - fft_size) / step_size;
 
@@ -78,6 +78,8 @@ fn main() -> eframe::Result {
                 texture_l: None,
                 texture_c: None,
                 input: input,
+                fft_size: FftSizes::F512,
+                overlap : 0.8,
             };
 
             Ok(Box::new(myapp))
@@ -91,13 +93,31 @@ struct MyApp {
     texture_r: Option<Vec<egui::TextureHandle>>,
     texture_c: Option<Vec<egui::TextureHandle>>,
     input: input::Input,
+    fft_size: FftSizes,
+    overlap : f32,
+}
+#[derive(PartialEq,Copy,Clone)]
+#[repr(i32)]
+enum FftSizes {
+    F256 = 256,
+    F512 = 512,
+    F1024 = 1024,
+    F2048 = 2048
+}
+
+impl Default for FftSizes {
+    fn default() -> Self {
+        return FftSizes::F512;
+    }
+
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let old_fft_size = self.fft_size;
             if self.texture_l == None {
-                let (im_l, im_r, im_c) = load_images( &self.input);
+                let (im_l, im_r, im_c) = load_images( &self.input, self.fft_size as usize, self.overlap);
                 let (num_tex_l, num_tex_r) = (
                     im_l.size[0] / TEXTURE_MAX_SIZE + 1,
                     im_r.size[0] / TEXTURE_MAX_SIZE + 1,
@@ -131,6 +151,28 @@ impl eframe::App for MyApp {
                 self.texture_r = Some(texture_r);
                 self.texture_c = Some(texture_c);
             }
+           
+            egui::ComboBox::from_label("FFT size")
+            .selected_text(format!("{}", self.fft_size as usize))
+            .show_ui(ui, |ui| {
+                ui.selectable_value(&mut self.fft_size, FftSizes::F256, "256");
+                ui.selectable_value(&mut self.fft_size, FftSizes::F512, "512");
+                ui.selectable_value(&mut self.fft_size, FftSizes::F1024, "1024");
+                ui.selectable_value(&mut self.fft_size, FftSizes::F2048, "2048");
+            });
+
+            
+            let old_overlap = self.overlap;
+            ui.add(egui::Slider::new(&mut self.overlap, 0.1..=0.9)
+                    .text("f32 demo slider").smart_aim(false));
+            if self.fft_size != old_fft_size || self.overlap != old_overlap {
+                self.texture_l = None;
+                self.texture_r = None;
+                self.texture_c = None;
+                println!("overlap: {} {}", old_overlap, self.overlap);
+            }
+            
+        ui.end_row();
 
             if let (Some(texture_l), Some(texture_r), Some(texture_c)) =
                 (&self.texture_l, &self.texture_r, &self.texture_c)
